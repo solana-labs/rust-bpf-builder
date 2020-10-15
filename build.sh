0959f2cd66b3dc0ba4443ccc10f2256317ebc766
Jack May <jack@solana.com>
2020-10-15 14:26:20 -0700

    Build in CI

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ set -ex
 rm -rf deploy
 mkdir -p deploy
 
-(cd linux && ./build.sh )
-cp linux/out/solana-rust-bpf-linux.tar.bz2 deploy
+if [ "$(uname)" == "Linux" ]; then
+  (cd linux && ./build.sh )
+  cp linux/out/solana-rust-bpf-linux.tar.bz2 deploy
+else
+  echo Warning: Local machine not linux, cannot build linux variant, skipping
+fi
 
 if [ "$(uname)" == "Darwin" ]; then
   (cd osx && ./build.sh )
